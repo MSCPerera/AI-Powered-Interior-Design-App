9bbb0600d18393d411972b2aa455416ca155cd2d
MSCPerera <164726262+MSCPerera@users.noreply.github.com>
2025-05-11 17:58:14 +0530

    Update App

--- ai-room-redesign/app/page.js
+++ ai-room-redesign/app/page.js
@@ -1,9 +1,11 @@
+import { Button } from "@/components/ui/button";
 import Image from "next/image";
 
 export default function Home() {
   return (
     <div>
       <h3>Hello World</h3>
+      <Button>Subscribe</Button>
     </div>
   );
 }
